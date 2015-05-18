c5031223dc439cc681aaf88f0402a269b5c0691f
uula <uula18@yahoo.de>
2015-05-18 19:33:37 +0200

    Dodanie projektu z logiczna częścią projektu oraz projekt testów

---
+++ Ekonometria.Test/DataReaderTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MethodOfGraphs;
+
+namespace Ekonometria.Test {
+    [TestClass]
+    public class DataReaderTest {
+        private DataReader dr;
+        [TestInitialize]
+        public void CreateDataReader() {
+            dr = new DataReader(@"C:\Users\ula\Dropbox\projekt-ekonometria\Dane_testowe2.txt");
+        }
+
+        [TestCleanup]
+        public void DisposeDataReader() {
+            dr.Dispose();
+        }
+
+        [TestMethod]
+        public void ReadOneLine() {
+            Assert.AreEqual("1	45	0,76	0,036402626", dr.ReadLine());
+        }
+        [TestMethod]
+        public void ReadSomeLines() {
+            Assert.AreEqual("1	45	0,76	0,036402626", dr.ReadLine());
+            Assert.AreEqual("2	46	0,471567741	0,088", dr.ReadLine());
+            Assert.AreEqual("3	47	1,76	0,30255951", dr.ReadLine());
+        }
+
+        [TestMethod]
+        public void ReadLastLine() {
+            string lastLine = "";
+            while (dr.hasNextLine())
+                lastLine = dr.ReadLine();
+            Assert.AreEqual("21	65	0,216860871	10,088", lastLine);
+        }
+
+        [TestMethod]
+        public void CountLines() {
+            Assert.AreEqual(21, dr.CountLines());
+        }
+
+        [TestMethod]
+        public void CreateYMatrix() {
+            int[] yMatrix = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
+            int[] testMatrix = dr.CreateYMatrix();
+            for (int i = 0; i < 21; ++i) {
+                Assert.AreEqual(yMatrix[i], testMatrix[i]);
+            }
+        }
+
+        [TestMethod]
+        public void LineOfXMatrix() {
+            double[] tmp = { 1, 45, 0.76, 0.036402626 };
+            double[] testXMatrix = dr.LineOfXMatrix();
+            for (int i = 0; i < 4; ++i) {
+                Assert.AreEqual(tmp[i], testXMatrix[i]);
+            }
+        }
+    }
+}
---
+++ MethodOfGraphs/DataReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MethodOfGraphs {
+    public class DataReader : IDisposable {
+        public string fileName = @"C:\Users\ula\Dropbox\projekt-ekonometria\Dane_testowe2.txt";
+        public StreamReader sr;
+        public int count = 0;
+        public int[] yMatrix;
+        public double[] xMatrix;
+
+        public DataReader(string fileName) {
+            this.fileName = fileName;
+            sr = File.OpenText(fileName);
+        }
+
+        public string ReadLine() {
+            return sr.ReadLine();
+        }
+
+        public void Dispose() {
+            sr.Dispose();
+        }
+
+        public bool hasNextLine() {
+            return !sr.EndOfStream;
+        }
+
+        public int CountLines() {
+            while (hasNextLine()) {
+                ReadLine();
+                count++;
+            }
+            return count;
+        }
+
+        public int[] CreateYMatrix() {
+            int length = CountLines();
+            yMatrix = new int[length];
+            for (int i = 0; i < length; ++i)
+                yMatrix[i] = i + 1;
+            return yMatrix;
+        }
+
+
+        public double[] LineOfXMatrix() {
+            int length = CountLines();
+            xMatrix = new double[length];
+            string[] tmp = new string[length];
+            string c = ReadLine();
+            tmp = c.Split('\t');
+            for (int i = 0; i < length; ++i) {
+                xMatrix[i] = Convert.ToDouble(tmp[i]);
+            }
+            return xMatrix;
+        }
+
+    }
+}
