c2858d6d91bfe2865fc9ebdc774a21f136248e6c
uula <uula18@yahoo.de>
2015-05-23 17:38:20 +0200

    Wyznaczenie macierzy W,R0 oraz R

---
+++ Ekonometria.Test/CalculationsTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MethodOfGraphs;
+
+namespace Ekonometria.Test {
+    [TestClass]
+    public class CalculationsTest{
+        private Calculations c;
+
+        [TestInitialize]
+        public void CreateCalculations() {
+            c = new Calculations();
+        }
+
+        [TestMethod]
+        public void CorrelationCoefficient() {
+            double[,] correlationMatrix = {
+                                            {1,1,0.301593795227882,0.631761642524286},
+                                            {1,1,0.301593795227882,0.631761642524286},
+                                            {0.301593795227882,0.301593795227882,1,-0.541379575882234},
+                                            {0.631761642524286,0.631761642524286,-0.541379575882234,1}
+                                          };
+            double[,] testedMatrix = c.CorrelationMatrix();
+            for (int i = 0; i < 4; ++i) {
+                for (int j = 0; j < 4; j++) {
+                    Assert.AreEqual(correlationMatrix[i, j] , testedMatrix[i, j],0.0000001);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void R0Matrix() {
+            double[] testMatrix={1,1,0.301593795227882,0.631761642524286};
+            double[] R0= c.CreationR0Matrix();
+            for(int i=0; i<4;++i){
+                Assert.AreEqual(testMatrix[i], R0[i],0.00001);
+            }
+        }
+
+        [TestMethod]
+        public void RMatrix() {
+            double[,] Rtest ={
+                                {1,1,0.301593795227882,0.631761642524286},
+                                {0.301593795227882,0.301593795227882,1,-0.541379575882234},
+                                {0.631761642524286, 0.631761642524286,-0.541379575882234,1}
+                             };
+            double[,] R = c.CreationRMatrix();
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 4; j++)
+                    Assert.AreEqual(Rtest[i, j], R[i, j], 0.000001);
+        }
+
+        [TestMethod]
+        public void CriticalValueOfTheCorrelationCoefficient() {
+            double Ralfa=c.CriticalValue();
+            Assert.AreEqual(0.432853514,Ralfa , 0.0000001);
+        }
+    }
+}
--- MethodOfGraphs/Calculations.cs
+++ MethodOfGraphs/Calculations.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms.DataVisualization.Charting;
 
 
 
 namespace MethodOfGraphs {
     public class Calculations {
         DataReader d = new DataReader(@"C:\Users\ula\Dropbox\projekt-ekonometria\Dane_testowe2.txt");
-        public double CorrelationBetweenYX1() {
+        int n = 21;
+
+        public double[,] W = new double[4, 4];
+        public double[,] R = new double[3, 4];
+
+        public double[,] CorrelationMatrix() {
 
             double[] y = d.TheCreationOfYMatrix();
             double[,] xMatrix = d.TheCreationOfXMatrix();
@@ -25,8 +31,53 @@ namespace MethodOfGraphs {
                 x3[j] = xMatrix[2, j];
             }
             var excelApplication = new Application();
-            var r = excelApplication.WorksheetFunction.Pearson(y,x1);
-            return r;
+            W[0, 0] = excelApplication.WorksheetFunction.Pearson(y, y);
+            W[0, 1] = excelApplication.WorksheetFunction.Pearson(y, x1);
+            W[0, 2] = excelApplication.WorksheetFunction.Pearson(y, x2);
+            W[0, 3] = excelApplication.WorksheetFunction.Pearson(y, x3);
+
+            W[1, 0] = excelApplication.WorksheetFunction.Pearson(y, x1);
+            W[1, 1] = excelApplication.WorksheetFunction.Pearson(x1, x1);
+            W[1, 2] = excelApplication.WorksheetFunction.Pearson(x1, x2);
+            W[1, 3] = excelApplication.WorksheetFunction.Pearson(x1, x3);
+
+            W[2, 0] = W[0, 2];
+            W[2, 1] = W[1, 2];
+            W[2, 2] = excelApplication.WorksheetFunction.Pearson(x2, x2);
+            W[2, 3] = excelApplication.WorksheetFunction.Pearson(x2, x3);
+
+            W[3, 0] = W[0, 3];
+            W[3, 1] = W[1, 3];
+            W[3, 2] = W[2, 3];
+            W[3, 3] = excelApplication.WorksheetFunction.Pearson(x3, x3);
+
+            return W;
+        }
+
+        public double[] CreationR0Matrix() {
+
+            double[] R0 = new double[4];
+            double[,] source = CorrelationMatrix();
+            for (int i = 0; i < R0.Length; i++)
+                R0[i] = source[0, i];
+            return R0;
+        }
+
+        public double[,] CreationRMatrix() {
+            double[,] pom= CorrelationMatrix();
+            for (int j = 0; j < 4; j++) {
+                R[0, j] = pom[1, j];
+                R[1, j] = pom[2, j];
+                R[2, j] = pom[3, j];
+            }
+            return R;
+        }
+
+        public double CriticalValue() {
+            var chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            double tAlfa;
+            tAlfa = chart.DataManipulator.Statistics.InverseTDistribution(0.05, n-2);
+            
         }
     }
 }
